d627bbcb8d7f1ae6cc4e13fb00552e235d25dab1
CN <ckddnjs554@naver.com>
2021-07-13 16:29:57 +0900

    Feat : OutStagramActivity setToken, initView 함수 구현

--- app/src/main/java/com/yoochangwons/outstagram/OutStagramActivity.kt
+++ app/src/main/java/com/yoochangwons/outstagram/OutStagramActivity.kt
@@ -1,5 +1,8 @@
 package com.yoochangwons.outstagram
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yoochangwons.outstagram.databinding.ActivityOutStagramBinding
@@ -13,6 +16,34 @@ class OutStagramActivity : AppCompatActivity() {
         binding = ActivityOutStagramBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+    }
+
+    fun initView() {
+        val userId = getId()
+        val passwordOne = getPasswordOne()
+        val passwordTwo = getPasswordTwo()
+    }
+
+    fun setOnClick() {
+        binding.signUp.setOnClickListener {
+
+        }
+
+        binding.loginUp.setOnClickListener {
+
+        }
+    }
+
+    fun register(activity: Activity) {
+
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    fun setToken(token: String) {
+        val sp = getSharedPreferences("login_sp", Context.MODE_PRIVATE)
+        val editor = sp.edit()
+
+        editor.putString("login_sp", token)
     }
 
     fun getId(): String {
