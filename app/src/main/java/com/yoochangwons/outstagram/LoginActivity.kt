fab17b82c76390947ad57f5253592bb7d33e381b
CN <ckddnjs554@naver.com>
2021-07-14 01:01:16 +0900

    Feat : LoginActivity 로그인 페이지 디자인 구성 및 메인 페이지 버튼 간격 변경

---
+++ app/src/main/java/com/yoochangwons/outstagram/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.yoochangwons.outstagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yoochangwons.outstagram.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
