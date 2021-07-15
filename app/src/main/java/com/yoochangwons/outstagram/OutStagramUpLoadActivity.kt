60574107cae0a90173cb3dd04897d0e9899fb01a
CN <ckddnjs554@naver.com>
2021-07-15 23:06:22 +0900

    Feat : OutStagramUpLoadActivity getPicture 함수 작성

--- app/src/main/java/com/yoochangwons/outstagram/OutStagramUpLoadActivity.kt
+++ app/src/main/java/com/yoochangwons/outstagram/OutStagramUpLoadActivity.kt
@@ -1,11 +1,23 @@
 package com.yoochangwons.outstagram
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 
 class OutStagramUpLoadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_out_stagram_up_load)
     }
+
+    fun getPicture() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        intent.type = "image/*"
+        startActivityForResult(intent, 1000)
+    }
+
+
 }
