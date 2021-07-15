9912444efecb87bc279dd00db660b65f46de3537
CN <ckddnjs554@naver.com>
2021-07-15 22:52:38 +0900

    Feat : content 추가 API 작성

--- app/src/main/java/com/yoochangwons/outstagram/ServiceNetworkRetrofit.kt
+++ app/src/main/java/com/yoochangwons/outstagram/ServiceNetworkRetrofit.kt
@@ -1,10 +1,9 @@
 package com.yoochangwons.outstagram
 
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
-import retrofit2.http.POST
+import retrofit2.http.*
 
 
 interface ServiceNetworkRetrofit {
@@ -26,4 +25,11 @@ interface ServiceNetworkRetrofit {
 
     @GET("/instagram/post/list/all/")
     fun getAllPosts(): Call<ArrayList<Post>>
+
+    @Multipart
+    @POST("/instagram/post")
+    fun upLoadPost(
+        @Part image: MultipartBody.Part,
+        @Part("content") requestBody: RequestBody
+    ) : Call<Post>
 }
