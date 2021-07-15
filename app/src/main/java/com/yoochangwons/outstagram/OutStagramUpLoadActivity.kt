a53f8ce4d381f9704dc6551e31a6b994ebe5423c
CN <ckddnjs554@naver.com>
2021-07-15 18:06:45 +0900

    Feat : UserInfoView 디자인 구성 및 각 뷰의 id 값 설정

---
+++ app/src/main/java/com/yoochangwons/outstagram/OutStagramUpLoadActivity.kt
@@ -0,0 +1,11 @@
+package com.yoochangwons.outstagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OutStagramUpLoadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_out_stagram_up_load)
+    }
+}
---
+++ app/src/main/java/com/yoochangwons/outstagram/OutStagrmLoginInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.yoochangwons.outstagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OutStagrmLoginInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_out_stagrm_login_info)
+    }
+}
