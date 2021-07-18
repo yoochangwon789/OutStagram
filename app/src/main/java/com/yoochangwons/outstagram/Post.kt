6cc339845c472fe307e89d99c059fa61f757e9d4
CN <ckddnjs554@naver.com>
2021-07-19 02:48:41 +0900

    Feat : Post class Serializable 추가

--- app/src/main/java/com/yoochangwons/outstagram/Post.kt
+++ app/src/main/java/com/yoochangwons/outstagram/Post.kt
@@ -1,7 +1,9 @@
 package com.yoochangwons.outstagram
 
+import java.io.Serializable
+
 class Post(
     var owner: String? = null,
     var content: String? = null,
     var image: String? = null
-)
+) : Serializable
